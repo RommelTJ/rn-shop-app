3d01610d4f2a49af6f3dc221a3d8ccb57270c237
Rommel Rico <me@rommelrico.com>
2019-10-16 07:19:09 -0700

    :sparkles: Store: Initial implementation of products reducer

---
+++ store/reducers/products.js
@@ -0,0 +1,11 @@
+const initialState = {
+  availableProducts: [],
+  userProducts: []
+};
+
+export default (state = initialState, action) => {
+  switch (action) {
+    default:
+      return state;
+  }
+}
