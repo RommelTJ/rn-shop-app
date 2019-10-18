94e6cb126d5ef3fce012e08b797146314032a0b1
Rommel Rico <me@rommelrico.com>
2019-10-18 07:26:47 -0700

    :lipstick: ProductItem: Styling fixes

--- components/shop/ProductItem.js
+++ components/shop/ProductItem.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { View, Text, Image, Button, StyleSheet } from 'react-native';
+import Colors from "../../constants/Colors";
 
 const ProductItem = (props) => {
   return (
     <View style={styles.product}>
       <Image style={styles.image} source={{uri: props.image}} />
-      <Text style={styles.title} >{props.title}</Text>
-      <Text style={styles.price} >${props.price.toFixed(2)}</Text>
+      <View style={styles.details}>
+        <Text style={styles.title} >{props.title}</Text>
+        <Text style={styles.price} >${props.price.toFixed(2)}</Text>
+      </View>
       <View style={styles.actions} >
-        <Button title="View Details" onPress={props.onViewDetail} />
-        <Button title="To Cart" onPress={props.onAddToCart} />
+        <Button title="View Details" color={Colors.primary} onPress={props.onViewDetail} />
+        <Button title="To Cart" color={Colors.primary} onPress={props.onAddToCart} />
       </View>
     </View>
   );
@@ -34,6 +37,11 @@ const styles = StyleSheet.create({
     width: "100%",
     height: "60%"
   },
+  details: {
+    alignItems: "center",
+    height: "15%",
+    padding: 10
+  },
   title: {
     fontSize: 18,
     marginVertical: 4
@@ -45,7 +53,9 @@ const styles = StyleSheet.create({
   actions: {
     flexDirection: "row",
     justifyContent: "space-between",
-    alignItems: "center"
+    alignItems: "center",
+    height: "25%",
+    paddingHorizontal: 20
   }
 });
 
