05924eb502fc21ef0d472218e23f729ba788193e
Rommel Rico <me@rommelrico.com>
2019-10-19 11:37:38 -0700

    :sparkles: ProductsOverviewScreen: Dispatching to detail screen

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -15,7 +15,9 @@ const ProductsOverviewScreen = (props) => {
             image={itemData.item.imageUrl}
             title={itemData.item.title}
             price={itemData.item.price}
-            onViewDetail={() => {}}
+            onViewDetail={() => {
+              props.navigation.navigate({routeName: "ProductDetail"});
+            }}
             onAddToCart={() => {}}
           />
         );
