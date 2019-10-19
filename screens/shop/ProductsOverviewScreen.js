4a5743d784d3d59dd8c56055cf891311d1a0e37a
Rommel Rico <me@rommelrico.com>
2019-10-19 11:44:58 -0700

    :sparkles: ProductsOverviewScreen: Setting the product title

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -19,7 +19,8 @@ const ProductsOverviewScreen = (props) => {
               props.navigation.navigate(
                 "ProductDetail",
                 {
-                  productId: itemData.item.id
+                  productId: itemData.item.id,
+                  productTitle: itemData.item.title
                 }
               );
             }}
