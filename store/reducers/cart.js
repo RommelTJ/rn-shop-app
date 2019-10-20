5d45373336a87450bb57ae9883ba3e6632ddb741
Rommel Rico <me@rommelrico.com>
2019-10-19 21:30:49 -0700

    :sparkles: Store: Cart Reducer

--- store/reducers/cart.js
+++ store/reducers/cart.js
@@ -1,4 +1,5 @@
 import {ADD_TO_CART} from "../actions/cart";
+import CartItem from "../../models/cartItem";
 
 const initialState = {
   items: {},
@@ -11,7 +12,29 @@ export default (state = initialState, action) => {
       const addedProduct = action.product;
       const productPrice = addedProduct.price;
       const productTitle = addedProduct.title;
-      return state;
+
+      if (state.items[addedProduct.id]) {
+        // We already have the item in the cart.
+        const updatedCartItem = new CartItem(
+          state.items[addedProduct.id].quantity + 1,
+          productPrice,
+          productTitle,
+          state.items[addedProduct.id].sum + productPrice
+        );
+        return {
+          ...state,
+          items: { ...state.items, [addedProduct.id]: updatedCartItem },
+          totalAmount: state.totalAmount + productPrice
+        };
+      } else {
+        // Adding a brand new item.
+        const newCartItem = new CartItem(1, productPrice, productTitle, productPrice);
+        return {
+          ...state,
+          items: { ...state.items, [addedProduct.id]: newCartItem },
+          totalAmount: state.totalAmount + productPrice
+        };
+      }
     default:
       return state;
   }
