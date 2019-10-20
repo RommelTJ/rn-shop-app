dbcd1030b2fc963b37a0559750d85e3761507079
Rommel Rico <me@rommelrico.com>
2019-10-20 11:32:48 -0700

    :sparkles: ProductsOverviewScreen: Adding the custom header button

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import { FlatList } from "react-native";
+import { FlatList, Platform } from "react-native";
 import { useSelector, useDispatch } from "react-redux";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+
 import ProductItem from "../../components/shop/ProductItem";
 import * as cartActions from "../../store/actions/cart";
+import CustomHeaderButton from "../../components/UI/HeaderButton";
 
 const ProductsOverviewScreen = (props) => {
   const products = useSelector(state => state.products.availableProducts);
@@ -37,7 +40,16 @@ const ProductsOverviewScreen = (props) => {
 };
 
 ProductsOverviewScreen.navigationOptions = {
-  headerTitle: "All Products"
+  headerTitle: "All Products",
+  headerRight: (
+    <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+      <Item
+        title="Cart"
+        iconName={Platform.OS === "android" ? "md-cart" : "ios-cart"}
+        onPress={() => {}}
+      />
+    </HeaderButtons>
+  )
 };
 
 export default ProductsOverviewScreen;
