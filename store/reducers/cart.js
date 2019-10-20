97d4e9804644963b104bb222f1fd2b3c0a377394
Rommel Rico <me@rommelrico.com>
2019-10-19 21:21:58 -0700

    :sparkles: Store: Cart Reducer

--- store/reducers/cart.js
+++ store/reducers/cart.js
@@ -1,8 +1,18 @@
+import {ADD_TO_CART} from "../actions/cart";
+
 const initialState = {
   items: {},
   totalAmount: 0
 };
 
 export default (state = initialState, action) => {
-  return state;
+  switch (action.type) {
+    case ADD_TO_CART:
+      const addedProduct = action.product;
+      const productPrice = addedProduct.price;
+      const productTitle = addedProduct.title;
+      return state;
+    default:
+      return state;
+  }
 };
