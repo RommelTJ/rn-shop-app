f5b10a4ef3702ca25e1a9000bcc113b2a69fcdb2
Rommel Rico <me@rommelrico.com>
2019-10-19 21:37:41 -0700

    :sparkles: ProductsDetailScreen: Dispatching the add to cart action

--- screens/shop/ProductDetailScreen.js
+++ screens/shop/ProductDetailScreen.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { ScrollView, View, Text, Image, Button, StyleSheet } from 'react-native';
-import {useSelector} from "react-redux";
+import {useSelector, useDispatch} from "react-redux";
 import Colors from "../../constants/Colors";
+import * as cartActions from "../../store/actions/cart";
 
 const ProductDetailScreen = (props) => {
   const productId = props.navigation.getParam("productId");
@@ -13,7 +14,13 @@ const ProductDetailScreen = (props) => {
     <ScrollView>
       <Image style={styles.image} source={{uri: selectedProduct.imageUrl}} />
       <View style={styles.actions}>
-        <Button color={Colors.primary} title="Add to Cart" onPress={() => {}} />
+        <Button
+          color={Colors.primary}
+          title="Add to Cart"
+          onPress={() => {
+            dispatch(cartActions.addToCart(selectedProduct))
+          }}
+        />
       </View>
       <Text style={styles.price} >${selectedProduct.price.toFixed(2)}</Text>
       <Text style={styles.description} >{selectedProduct.description}</Text>
