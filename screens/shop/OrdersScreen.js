32550d4fe31361365b45832fb5ba446dd9099abf
Rommel Rico <me@rommelrico.com>
2019-10-23 07:13:43 -0700

    :sparkles: OrdersScreen: Setting up the drawer buttons

--- screens/shop/OrdersScreen.js
+++ screens/shop/OrdersScreen.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { FlatList, Text } from "react-native";
+import {FlatList, Platform, Text} from "react-native";
 import { useSelector } from "react-redux";
+import {HeaderButtons, Item} from "react-navigation-header-buttons";
+import CustomHeaderButton from "../../components/UI/HeaderButton";
 
 const OrdersScreen = (props) => {
   const orders = useSelector(state => state.orders.orders);
@@ -15,8 +17,21 @@ const OrdersScreen = (props) => {
   );
 };
 
-OrdersScreen.navigationOptions = {
-  headerTitle: 'Your Orders'
+OrdersScreen.navigationOptions = (navData) => {
+  return {
+    headerTitle: 'Your Orders',
+    headerLeft: (
+      <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+        <Item
+          title="Menu"
+          iconName={Platform.OS === "android" ? "md-menu" : "ios-menu"}
+          onPress={() => {
+            navData.navigation.toggleDrawer();
+          }}
+        />
+      </HeaderButtons>
+    )
+  };
 };
 
 export default OrdersScreen;
