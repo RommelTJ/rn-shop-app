25611b96e4606a5d2b585c3d5602d72bc4f892b0
Rommel Rico <me@rommelrico.com>
2019-10-26 09:17:58 -0700

    :sparkles: ShopNavigator: Navigating to the UserProductsScreen

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -10,6 +10,7 @@ import Colors from "../constants/Colors";
 import ProductDetailScreen from "../screens/shop/ProductDetailScreen";
 import CartScreen from "../screens/shop/CartScreen";
 import OrdersScreen from "../screens/shop/OrdersScreen";
+import UserProductsScreen from "../screens/user/UserProductsScreen";
 
 const defaultNavOptions = {
   headerStyle: {
@@ -65,10 +66,30 @@ const OrdersNavigator = createStackNavigator(
   }
 );
 
+const AdminNavigator = createStackNavigator(
+  {
+    UserProducts: UserProductsScreen
+  }, {
+    navigationOptions: {
+      drawerIcon: drawerConfig => {
+        return (
+          <Ionicons
+            name={Platform.OS === 'android' ? "md-create" : "ios-create"}
+            size={23}
+            color={drawerConfig.tintColor}
+          />
+        );
+      }
+    },
+    defaultNavigationOptions: defaultNavOptions
+  }
+);
+
 const ShopNavigator = createDrawerNavigator(
   {
     Products: ProductsNavigator,
-    Orders: OrdersNavigator
+    Orders: OrdersNavigator,
+    Admin: AdminNavigator
   }, {
     contentOptions: {
       activeTintColor: Colors.primary
