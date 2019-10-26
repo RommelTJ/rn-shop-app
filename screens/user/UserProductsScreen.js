5c567e145feee555877d29e8b96a5282f38cef6a
Rommel Rico <me@rommelrico.com>
2019-10-26 09:19:07 -0700

    :bug: UserProductsScreen: Typo

--- screens/user/UserProductsScreen.js
+++ screens/user/UserProductsScreen.js
@@ -5,7 +5,7 @@ import { useSelector } from "react-redux";
 import ProductItem from "../../components/shop/ProductItem";
 
 const UserProductsScreen = (props) => {
-  const userProducts = useSelector(state => state.product.userProducts);
+  const userProducts = useSelector(state => state.products.userProducts);
   return (
     <FlatList
       data={userProducts}
