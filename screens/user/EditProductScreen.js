1901cfdb0302ad65d518583c8a894801616a7d48
Rommel Rico <me@rommelrico.com>
2019-10-27 16:16:31 -0700

    :sparkles: EditProductScreen: Saving user input

--- screens/user/EditProductScreen.js
+++ screens/user/EditProductScreen.js
@@ -1,27 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {ScrollView, View, Text, TextInput, StyleSheet, Platform} from 'react-native';
 import CustomHeaderButton from "../../components/UI/HeaderButton";
 import {HeaderButtons, Item} from "react-navigation-header-buttons";
 
 const EditProductScreen = (props) => {
+  const [title, setTitle] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
+  const [price, setPrice] = useState('');
+  const [description, setDescription] = useState('');
+
   return (
     <ScrollView>
       <View style={styles.form}>
         <View style={styles.formControl} >
           <Text style={styles.label}>Title</Text>
-          <TextInput style={styles.input} />
+          <TextInput style={styles.input} value={title} onChangeText={text => setTitle(text)} />
         </View>
         <View style={styles.formControl} >
           <Text style={styles.label}>Image URL</Text>
-          <TextInput style={styles.input} />
+          <TextInput style={styles.input} value={imageUrl} onChangeText={text => setImageUrl(text)} />
         </View>
         <View style={styles.formControl} >
           <Text style={styles.label}>Price</Text>
-          <TextInput style={styles.input} />
+          <TextInput style={styles.input} value={price} onChangeText={text => setPrice(text)} />
         </View>
         <View style={styles.formControl} >
           <Text style={styles.label}>Description</Text>
-          <TextInput style={styles.input} />
+          <TextInput style={styles.input} value={description} onChangeText={text => setDescription(text)} />
         </View>
       </View>
     </ScrollView>
