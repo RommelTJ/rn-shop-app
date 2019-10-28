ef4c623307b574f497862635fc3d8822f860c600
Rommel Rico <me@rommelrico.com>
2019-10-28 07:15:54 -0700

    :sparkles: Store: Handling Creating/Updating in Products Reducer

--- store/reducers/products.js
+++ store/reducers/products.js
@@ -1,5 +1,6 @@
 import PRODUCTS from "../../data/dummy-data";
-import {DELETE_PRODUCT} from "../actions/products";
+import {CREATE_PRODUCT, DELETE_PRODUCT, UPDATE_PRODUCT} from "../actions/products";
+import Product from "../../models/product";
 
 const initialState = {
   availableProducts: PRODUCTS,
@@ -8,6 +9,40 @@ const initialState = {
 
 export default (state = initialState, action) => {
   switch (action.type) {
+    case CREATE_PRODUCT:
+      const newProduct = new Product(
+        new Date().toString(),
+        'u1',
+        action.productData.title,
+        action.productData.imageUrl,
+        action.productData.description,
+        action.productData.price
+      );
+      return {
+        ...state,
+        availableProducts: state.availableProducts.concat(newProduct),
+        userProducts: state.userProducts.concat(newProduct)
+      };
+    case UPDATE_PRODUCT:
+      const productIdx = state.userProducts.findIndex(p => p.id === action.pid);
+      const availableProductIdx = state.availableProducts.findIndex(p => p.id === action.pid);
+      const updatedProduct = new Product(
+        action.pid,
+        state.userProducts[productIdx].ownerId,
+        action.productData.title,
+        action.productData.imageUrl,
+        action.productData.description,
+        state.userProducts[productIdx].price
+      );
+      const updatedUserProducts = [...state.userProducts];
+      updatedUserProducts[productIdx] = updatedProduct;
+      const updatedAvailableProducts = [...state.availableProducts];
+      updatedAvailableProducts[availableProductIdx] = updatedProduct;
+      return {
+        ...state,
+        availableProducts: updatedAvailableProducts,
+        userProducts: updatedUserProducts
+      };
     case DELETE_PRODUCT:
       return {
         ...state,
