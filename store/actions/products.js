a32f7f9597ffae74edd5a53e71139193225248b5
Rommel Rico <me@rommelrico.com>
2019-10-28 07:16:20 -0700

    :bug: Store: Products updates shouldn't include price

--- store/actions/products.js
+++ store/actions/products.js
@@ -10,6 +10,6 @@ export const createProduct = (title, description, imageUrl, price) => {
   return { type: CREATE_PRODUCT, productData: {title, description, imageUrl, price} };
 };
 
-export const updateProduct = (id, title, description, imageUrl, price) => {
-  return { type: UPDATE_PRODUCT, pid: id, productData: {title, description, imageUrl, price} };
+export const updateProduct = (id, title, description, imageUrl) => {
+  return { type: UPDATE_PRODUCT, pid: id, productData: {title, description, imageUrl} };
 };
