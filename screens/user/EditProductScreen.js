75372c29ea3ff85c1f7a4236148b49b80b1230de
Rommel Rico <me@rommelrico.com>
2019-10-29 05:03:15 -0700

    :sparkles: EditProductScreen: Auto-capitalize the title

--- screens/user/EditProductScreen.js
+++ screens/user/EditProductScreen.js
@@ -34,7 +34,14 @@ const EditProductScreen = (props) => {
       <View style={styles.form}>
         <View style={styles.formControl} >
           <Text style={styles.label}>Title</Text>
-          <TextInput style={styles.input} value={title} onChangeText={text => setTitle(text)} keyboardType='default' />
+          <TextInput
+            style={styles.input}
+            value={title}
+            onChangeText={text => setTitle(text)}
+            keyboardType='default'
+            autoCapitalize='sentences'
+            autoCorrect
+          />
         </View>
         <View style={styles.formControl} >
           <Text style={styles.label}>Image URL</Text>
