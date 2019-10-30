ff3efc674afa1cfc0e3efed7eae7cad590873c93
Rommel Rico <me@rommelrico.com>
2019-10-30 06:51:32 -0700

    :tada: Input: Initial commit

---
+++ components/UI/Input.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text, View, TextInput, StyleSheet } from "react-native";
+
+const Input = (props) => {};
+
+const styles = StyleSheet.create({});
+
+export default Input;
