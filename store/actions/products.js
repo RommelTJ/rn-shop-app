9e1a9b74b22314022b4a05ac978e533d419a627d
Rommel Rico <me@rommelrico.com>
2019-10-31 07:14:02 -0700

    :sparkles: Store: Dispatching createProduct action using Redux Thunk syntax

--- store/actions/products.js
+++ store/actions/products.js
@@ -7,7 +7,21 @@ export const deleteProduct = (productId) => {
 };
 
 export const createProduct = (title, description, imageUrl, price) => {
-  return { type: CREATE_PRODUCT, productData: {title, description, imageUrl, price} };
+  return async (dispatch) => {
+    // any async code you want!
+    const response = await fetch('https://REDACTED.firebaseio.com/products.json', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({title, description, imageUrl, price})
+    });
+
+    const responseData = await response.json();
+    console.log(responseData);
+
+    dispatch({ type: CREATE_PRODUCT, productData: {title, description, imageUrl, price} });
+  };
 };
 
 export const updateProduct = (id, title, description, imageUrl) => {
