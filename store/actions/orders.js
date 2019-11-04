986c5aab7bb7134870a15b9f05e4d2ad3ef5a116
Rommel Rico <me@rommelrico.com>
2019-11-03 17:20:42 -0800

    :sparkles: Store: Orders action to add orders to firebase

--- store/actions/orders.js
+++ store/actions/orders.js
@@ -1,5 +1,24 @@
 export const ADD_ORDER = "ADD_ORDER";
 
+const BASE_URL = "https://REDACTED.firebaseio.com";
+
 export const addOrder = (cartItems, totalAmount) => {
-  return { type: ADD_ORDER, orderData: {items: cartItems, amount: totalAmount}};
+  return async (dispatch) => {
+    // any async code you want!
+    const date = new Date();
+    const response = await fetch(`${BASE_URL}/orders/u1.json`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({cartItems, totalAmount, date: date.toISOString()})
+    });
+
+    if (!response.ok) {
+      throw new Error("Something went wrong!");
+    }
+    const responseData = await response.json();
+
+    dispatch({ type: ADD_ORDER, orderData: { id: responseData.name, items: cartItems, amount: totalAmount, date } });
+  };
 };
