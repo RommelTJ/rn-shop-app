b8c65b6ba8d9d964e0bba747c7de82e53071cbd0
Rommel Rico <me@rommelrico.com>
2019-11-03 17:39:21 -0800

    :sparkles: OrdersScreen: Dispatching set orders

--- screens/shop/OrdersScreen.js
+++ screens/shop/OrdersScreen.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {FlatList, Platform, Text} from "react-native";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import {HeaderButtons, Item} from "react-navigation-header-buttons";
 import CustomHeaderButton from "../../components/UI/HeaderButton";
 import OrderItem from "../../components/shop/OrderItem";
+import * as ordersActions from "../../store/actions/orders";
+
 
 const OrdersScreen = (props) => {
   const orders = useSelector(state => state.orders.orders);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(ordersActions.fetchOrders());
+  }, [dispatch]);
+
   return (
     <FlatList
       data={orders}
