299a8c611c79d324c71b6f30328ed8520fab5d19
Rommel Rico <me@rommelrico.com>
2019-11-04 07:25:48 -0800

    :lipstick: AuthScreen: Some more styling

--- screens/user/AuthScreen.js
+++ screens/user/AuthScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ScrollView, StyleSheet, KeyboardAvoidingView, Button } from 'react-native';
+import { ScrollView, StyleSheet, KeyboardAvoidingView, Button, View } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 
 import Input from "../../components/UI/Input";
@@ -35,16 +35,20 @@ const AuthScreen = (props) => {
               onInputChange={() => {}}
               initialValue=""
             />
-            <Button
-              title="Login"
-              color={Colors.primary}
-              onPress={() => {}}
-            />
-            <Button
-              title="Switch to Sign Up"
-              color={Colors.accent}
-              onPress={() => {}}
-            />
+            <View style={styles.buttonContainer}>
+              <Button
+                title="Login"
+                color={Colors.primary}
+                onPress={() => {}}
+              />
+            </View>
+            <View style={styles.buttonContainer}>
+              <Button
+                title="Switch to Sign Up"
+                color={Colors.accent}
+                onPress={() => {}}
+              />
+            </View>
           </ScrollView>
         </Card>
       </LinearGradient>
@@ -70,6 +74,9 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  buttonContainer: {
+    marginTop: 10
   }
 });
 
