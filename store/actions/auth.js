0a01dd4d94f0ed44102be91e78015065c998202a
Rommel Rico <me@rommelrico.com>
2019-11-06 06:45:43 -0800

    :sparkles: Store: Sign In Action Creator

--- store/actions/auth.js
+++ store/actions/auth.js
@@ -1,4 +1,5 @@
 export const SIGN_UP = "SIGN_UP";
+export const SIGN_IN = "SIGN_IN";
 
 const API_KEY = 'REDACTED';
 
@@ -17,7 +18,25 @@ export const signUp = (email, password) => {
     }
 
     const resData = await response.json();
-    console.log("resData: ", resData);
     dispatch({type: SIGN_UP});
+  };
+};
+
+export const logIn = (email, password) => {
+  return async(dispatch) => {
+    const response = await fetch(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${API_KEY}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({email, password, returnSecureToken: true})
+    });
+
+    if (!response.ok) {
+      throw new Error("Something went wrong!");
+    }
+
+    const resData = await response.json();
+    dispatch({type: SIGN_IN});
   };
 };
