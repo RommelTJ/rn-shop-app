f67e3f140d8ae143f78d1baede8507b5b8ccd1e5
Rommel Rico <me@rommelrico.com>
2019-11-06 21:07:38 -0800

    :sparkles: OrdersScreen: Showing error messages

--- screens/shop/OrdersScreen.js
+++ screens/shop/OrdersScreen.js
@@ -26,6 +26,14 @@ const OrdersScreen = (props) => {
     );
   }
 
+  if (orders.length === 0) {
+    return (
+      <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
+        <Text>No orders found. Maybe start ordering some products.</Text>
+      </View>
+    );
+  }
+
   return (
     <FlatList
       data={orders}
