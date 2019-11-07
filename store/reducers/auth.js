83eca349701224c5fccb4a8e197975eec2f45322
Rommel Rico <me@rommelrico.com>
2019-11-06 20:30:26 -0800

    :sparkles: Store: Setting up the auth reducer

--- store/reducers/auth.js
+++ store/reducers/auth.js
@@ -1,4 +1,22 @@
+import { SIGN_UP, SIGN_IN } from "../actions/auth";
+
 const initialState = {
   token: null,
   userId: null
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case SIGN_IN:
+      return {
+        token: action.token,
+        userId: action.userId
+      };
+    case SIGN_UP:
+      return {
+        token: action.token,
+        userId: action.userId
+      };
+    default: return state;
+  }
 };
