06e614cff6b751464a0434f1e54d41bc1785c21d
Rommel Rico <me@rommelrico.com>
2019-11-07 07:21:38 -0800

    :sparkles: App: Using the NavigationContainer

--- App.js
+++ App.js
@@ -6,11 +6,10 @@ import * as Font from "expo-font";
 import ReduxThunk from 'redux-thunk';
 import authReducer from "./store/reducers/auth";
 
-import ShopNavigator from "./navigation/ShopNavigator";
-
 import productsReducer from './store/reducers/products';
 import cartReducer from './store/reducers/cart';
 import ordersReducer from './store/reducers/orders';
+import NavigationContainer from "./navigation/NavigationContainer";
 
 const rootReducer = combineReducers({
   products: productsReducer,
@@ -34,7 +33,7 @@ export default function App() {
 
   return (
     <Provider store={store}>
-      <ShopNavigator />
+      <NavigationContainer />
     </Provider>
   );
 }
